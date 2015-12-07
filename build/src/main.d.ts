cadfa84e7bfde4bd0fba41e6bb945c2e7c0edb88
许阳 <gastrodia@outlook.com>
2015-12-07 17:39:22 +0800

    WonderVR base code

---
+++ build/src/main.d.ts
@@ -0,0 +1,7 @@
+declare var scene: THREE.Scene;
+declare var camera: THREE.PerspectiveCamera;
+declare var renderer: THREE.WebGLRenderer;
+declare var geometry: THREE.BoxGeometry;
+declare var material: THREE.MeshBasicMaterial;
+declare var cube: THREE.Mesh;
+declare var render: () => void;
