e3ed2549366df8151359441229d441966ab4117d
wugl <wglookk@126.com>
2015-12-25 11:11:35 +0800

    点击图片 场景生成对象2

---
+++ src/MainUI.ts
@@ -0,0 +1,26 @@
+import WonderVR from './WonderVR';
+export default class MainUI{
+  constructor(wonder:WonderVR){
+    $(".box-button-leftcon").find("ul.json").find("li").click(function(){
+      //console.log(this.id);
+        var id=$(this).attr("id");
+        var name=$(this).attr("name");
+        var num = $(this).index()
+        var htmlObj = $.ajax({url:"./demo.txt",async:false});
+        var OhtmlObj=htmlObj.responseText.split(",");
+        var ObjStr = OhtmlObj[num];
+//alert(name)
+      wonder.scene.addObject({
+        objUrl:'./resource/obj/' + ObjStr + '.obj',
+        mtlUrl:'./resource/obj/' + ObjStr + '.mtl',
+        position:{x:500,y:500,z:500},
+        name:name,
+        id:id
+      });
+
+
+
+      //
+    })
+  }
+}
